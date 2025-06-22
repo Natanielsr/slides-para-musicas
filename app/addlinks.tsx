80998a34e8ae99ce41294e51d269e08bee7d81b9
¨Nataniel¨ <contatonatanielsoares@gmail.com>
2025-06-21 21:54:19 -0300

    addlinks page

---
+++ app/addlinks.tsx
@@ -0,0 +1,71 @@
+import { globalStyles } from "@/constants/globalStyles";
+import { StyleSheet, TextInput, View } from "react-native";
+import { useRef, useState, useContext } from 'react';
+import BackButton from "@/components/BackButton";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { GlobalListContext } from "@/global/GlobalListProvider";
+import ButtonGPT from '@/components/ButtonGPT'
+import ToastMessage, {ToastMessageHandle} from "@/components/ToastMessage";
+
+
+
+export default function SearchMusic(){
+    const [inputLink, setInputLink] = useState('');
+    const context = useContext(GlobalListContext);
+    const toastRef = useRef<ToastMessageHandle | null>(null);
+
+    const showToast = (message: string) => {
+        if (toastRef.current) {
+            toastRef.current.show({ message, duration: 3000 });
+        }
+    }
+
+    if(!context){
+        throw new Error('SearchMusic must be used within a GlobalListProvider');
+    }
+    const { addItem } = context;
+
+    const handleAddLink = () => {
+        if (inputLink.trim() !== '') {
+            addItem({'name': 'link', 'link': inputLink});
+            showToast('Link Adicionado!')
+        }
+        else{
+            showToast('Link inválido!')
+        }
+    };
+
+    return (
+    <SafeAreaView style={globalStyles.container}>
+        <BackButton></BackButton>
+        <TextInput 
+            autoFocus={true}
+            style={styles.input}
+            placeholder={'Adicionar um Link'}
+            value={inputLink}
+            onChangeText={setInputLink}
+            />
+        <View style={styles.btn}>
+            <ButtonGPT title="Adicionar" onPress={handleAddLink} iconName="add"/>
+        </View>
+
+        <ToastMessage ref={toastRef}></ToastMessage>
+    </SafeAreaView>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        width: '100%',
+        color: '#000',
+        backgroundColor: '#FFF',
+        borderRadius: 60,
+        borderColor: '#000000',
+        borderWidth: 2,
+    },
+    btn :{
+        paddingTop: 20
+    }
+});
